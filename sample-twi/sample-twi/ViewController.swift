560f1518106fee91b9b9f9d72006267d3b13be1c
Alexandru Plugaru <alexandru@gymglish.com>
2014-10-14 15:14:47 +0200

    Added SceneKit stuff

--- sample-twi/sample-twi/ViewController.swift
+++ sample-twi/sample-twi/ViewController.swift
@@ -1,5 +1,7 @@
 import UIKit
 import CoreBluetooth
+import QuartzCore
+import SceneKit
 
 class ViewController: UIViewController, CBCentralManagerDelegate {
     
@@ -8,7 +10,93 @@ class ViewController: UIViewController, CBCentralManagerDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+       
+        // create a new scene
+        let scene = SCNScene() //(named: "art.scnassets/ship.dae")
+        
+        // create and add a camera to the scene
+        let cameraNode = SCNNode()
+        cameraNode.camera = SCNCamera()
+        scene.rootNode.addChildNode(cameraNode)
+        
+        // place the camera
+        cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
+        
+        // create and add a light to the scene
+        let lightNode1 = SCNNode()
+        lightNode1.light = SCNLight()
+        lightNode1.light!.type = SCNLightTypeOmni
+        lightNode1.light!.color = UIColor.whiteColor()
+        lightNode1.position = SCNVector3(x: 0, y: 10, z: 10)
+        scene.rootNode.addChildNode(lightNode1)
+        
+        //let lightNode2 = SCNNode()
+        //lightNode2.light = SCNLight()
+        //lightNode2.light!.type = SCNLightTypeOmni
+        //lightNode2.light!.color = UIColor.purpleColor()
+        //lightNode2.position = SCNVector3(x: 10, y: 1, z: 1)
+        //scene.rootNode.addChildNode(lightNode2)
+        //
+        //let lightNode3 = SCNNode()
+        //lightNode3.light = SCNLight()
+        //lightNode3.light!.type = SCNLightTypeOmni
+        //lightNode3.light!.color = UIColor.yellowColor()
+        //lightNode3.position = SCNVector3(x: -20, y: -10, z: 10)
+        //scene.rootNode.addChildNode(lightNode3)
+        
+        // create and add an ambient light to the scene
+        let ambientLightNode = SCNNode()
+        ambientLightNode.light = SCNLight()
+        ambientLightNode.light!.type = SCNLightTypeAmbient
+        ambientLightNode.light!.color = UIColor.darkGrayColor()
+        scene.rootNode.addChildNode(ambientLightNode)
+        
+        
+        let box = SCNBox(width: 3, height: 3, length: 3, chamferRadius: 0.1)
+        let boxMaterial = SCNMaterial()
+        boxMaterial.reflective.contents = UIImage(named: "sphere.png")
+        box.materials?.append(boxMaterial)
+        
+        let boxNode = SCNNode(geometry: box)
+        
+        // Add animation
+        let spin = CABasicAnimation(keyPath: "rotation")
+        spin.toValue = NSValue(SCNVector4: SCNVector4(x: 1, y: 1.0, z: 0.0, w: Float(2.0*M_PI)))
+        spin.duration = 3
+        spin.repeatCount = HUGE // for infinity
+        boxNode.addAnimation(spin, forKey: "spin around")
+        
+        scene.rootNode.addChildNode(boxNode)
+        
+        
+        // retrieve the SCNView
+        let scnView = self.view as SCNView
+        
+        // set the scene to the view
+        scnView.scene = scene
+        
+        // allows the user to manipulate the camera
+        scnView.allowsCameraControl = true
+        
+        // show statistics such as fps and timing information
+        // scnView.showsStatistics = true
+        
+        // configure the view
+        scnView.backgroundColor = UIColor.blackColor()
+        
         startUpCentralManager()
+    }
+    
+    override func shouldAutorotate() -> Bool {
+        return true
+    }
+    
+    override func supportedInterfaceOrientations() -> Int {
+        if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
+            return Int(UIInterfaceOrientationMask.AllButUpsideDown.toRaw())
+        } else {
+            return Int(UIInterfaceOrientationMask.All.toRaw())
+        }
     }
     
     func startUpCentralManager() {
